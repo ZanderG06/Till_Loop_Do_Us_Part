7ed96f8ceb288be226e620541f95ebf98f6bbcdb
Zander Goodwin <zanderngoodwin@gmail.com>
2026-05-17 20:24:35 -0300

    Credits added

--- Assets/Scripts/UiButtons.cs
+++ Assets/Scripts/UiButtons.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class UiButtons : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject credits;
+
     public void ChangeScene(int scene)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
@@ -10,5 +13,17 @@ public class UiButtons : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        credits.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
